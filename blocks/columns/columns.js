ff1ceb7044ff9986f1c2cfc0defb9127ab98475a
amarghioali <88341119+amarghioali@users.noreply.github.com>
2026-07-31 13:56:02 +0300

    Address code review comments

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,10 +2,6 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
-  // external Dynamic Media images are decorated globally in decorateMain
-  // (scripts/dm-images.js)
-
-  // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
