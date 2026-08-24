59ebcf25bddff18e63aa818a4b84bbde87d89397
ShivaniM123 <shivanimaheshwari404@gmail.com>
2026-08-24 13:43:14 +0530

    lass name fixed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -396,8 +396,6 @@ export async function loadTemplate(doc, templateName) {
       })();
     });
 
-    document.body.classList.add(`${templateName}-template`);
-
     await Promise.all([cssLoaded, decorationComplete]);
   } catch (error) {
     // eslint-disable-next-line no-console
